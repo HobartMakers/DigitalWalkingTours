6de5aed218a21fc8aa457c71a0c14f1f4d6123bd
Alastair Taft <ali.c.taft@gmail.com>
2017-07-28 23:01:10 +1000

    Integreated the leaflet map.

--- web/src/App.js
+++ web/src/App.js
@@ -7,8 +7,8 @@ const styles = {
   app: {
     width: '100vw',
     height: '100vh',
-    backgroundImage: `url(${require('./testMap.png')})`,
-    backgroundSize: 'cover',
+    //backgroundImage: `url(${require('./testMap.png')})`,
+    //backgroundSize: 'cover',
   },
   buttonContainer: {
     textAlign: 'center',
@@ -16,6 +16,7 @@ const styles = {
     bottom: 0,
     height: 0,
     width: '100vw',
+    zIndex: 401,
   },
   buttonContainerInner: {
     position: 'relative',
@@ -25,13 +26,31 @@ const styles = {
     margin: '0 auto',
     top: -(48 + 20),
   },
+  mapContainer: {
+    width: '100vw',
+    height: '100vh',
+  },
 }
 
 class App extends Component {
+
+  componentDidMount = () => {
+    var mymap = window.L.map('map').setView([-42.87, 147.25], 10);
+
+    window.L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+            '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+            'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        id: 'mapbox.streets'
+    }).addTo(mymap)
+  };
+
   render() {
     const { classes } = this.props
     return (
       <div className={classes.app}>
+        <div className={classes.mapContainer} id="map" />
         <div className={classes.buttonContainer}>
           <div className={classes.buttonContainerInner}>
             <Button className={classes.startTourButton} />
