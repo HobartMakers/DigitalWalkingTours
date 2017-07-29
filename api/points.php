0350e673a5de27be69a64cb4102a5aadcd5ee1d1
Craig Marshall <craig9@gmail.com>
2017-07-29 14:50:57 +1000

    lon -> long

--- api/points.php
+++ api/points.php
@@ -49,7 +49,7 @@ foreach ($hobart_facilities['features'] as $f) {
     $feature = [];
 
     $feature['title'] = $f['properties']['NAME'];
-    $feature['lon'] = $f['geometry']['coordinates'][0];
+    $feature['long'] = $f['geometry']['coordinates'][0];
     $feature['lat'] = $f['geometry']['coordinates'][1];
     $type = $f['properties']['COM_TYPE1'] . " / " . $f['properties']['COM_TYPE2'];
 
@@ -72,7 +72,7 @@ foreach ($urban_art['features'] as $f) {
     $feature = [];
 
     $feature['title'] = $f['properties']['Title'];
-    $feature['lon'] = $f['geometry']['coordinates'][0];
+    $feature['long'] = $f['geometry']['coordinates'][0];
     $feature['lat'] = $f['geometry']['coordinates'][1];
     $feature['type'] = 'urban art';
     $feature['contents'] = "Artist: " . $f['properties']['Artist'] . "\n" . "Date: " . $f['properties']['Date'];
@@ -97,5 +97,9 @@ $filtered = array_values(array_filter($features, function($var) {
 //            'features' => $filtered];
 
 header('Content-type: application/json');
+$origin = rtrim($_SERVER['HTTP_REFERER'], "/");
+//$origin = $_SERVER['REMOTE_ADDR'];
+header("Access-Control-Allow-Origin: $origin");
+header("Access-Control-Allow-Headers: Content-Type");
 echo json_encode($filtered);
 
